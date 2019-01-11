6ba41e2d6eae9df3e642c489de17d99478d5f143
000030386 <000030386@student.thomroth.ac.uk>
2019-01-11 10:27:22 +0000

    2016 Q4

--- 2012 Q6/2012 Q6/Program.cs
+++ 2012 Q6/2012 Q6/Program.cs
@@ -10,7 +10,15 @@ namespace _2012_Q6
     {
         static void Main(string[] args)
         {
-            int answer = 0;
+         int answer = 0;
+         int column = 8;
+         int bit = 0;
+
+
+            Console.WriteLine("Enter bit value: ");
+            bit = Convert.ToInt32(Console.ReadLine());
+            answer = answer + (column / bit);
+            column = (column / 2);
         }
     }
 }
---
+++ 2013 Q4/2013 Q4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2013_Q4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbertoguess = 0;
+            int numberofguesses = 0;
+            int guess = 0;
+            Console.WriteLine("Player one enter your chosen number: ");
+            numbertoguess = Convert.ToInt32(Console.ReadLine());
+
+            while (numbertoguess < 1 || numbertoguess > 10) ;
+            {
+                Console.WriteLine("Not a valid choice, please enter another guess");
+                numbertoguess = Convert.ToInt32(Console.ReadLine());
+            }
+
+            guess = 0;
+            numberofguesses = 0;
+
+            while (guess != numberofguesses && numberofguesses < 5) do;
+                {
+                    Console.WriteLine("Player two have a guess: ");
+                    guess = Convert.ToInt32(Console.ReadLine());
+                    numberofguesses = numberofguesses + 1;
+                }
+
+            if (guess = numberofguesses)
+            {
+
+            }
+        }
+    }
+}
